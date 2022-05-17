a6e0a715d5fca639456ec37c19dd5be36f72f76b
Robert Adams <misterblue@misterblue.com>
2022-05-17 16:45:24 +0000

    Bump version to 2.0.4

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -3,8 +3,8 @@
 using System;
 namespace org.herbal3d.cs.CommonUtil {
     public class VersionInfo {
-        public static string appVersion = "2.0.3";
-        public static string longVersion = "2.0.3-20220515-361c59ba";
-        public static string buildDate = "20220515";
+        public static string appVersion = "2.0.4";
+        public static string longVersion = "2.0.4-20220517-598098ba";
+        public static string buildDate = "20220517";
     }
 }
